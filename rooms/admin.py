8e469836cd07aa213cdcb38b694ec3ace7da08fc
lisa <lisa.exz@gmail.com>
2021-02-17 12:11:41 +0200

    Add reletionships in Model Room

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(
+    models.RoomType,
+    models.Amenity,
+    models.Fasility,
+    models.HouseRule,
+    models.Rule
+)
+class ItemAdmin(admin.ModelAdmin):
+    """ Item Admin Definition """
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
-    pass
+    """ Room Admin Definition """
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """ Photo Admin Definition """
--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,50 @@
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
-from users import models as user_models
+
+
+class AbstractItem(core_models.TimeStampedModel):
+    """ Absract Item """
+
+    name = models.CharField(max_length=140)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        abstract = True
+
+
+class RoomType(AbstractItem):
+    """ Room Type Model Definition """
+
+    class Meta:
+        verbose_name = "Room Type"
+
+
+class Amenity(AbstractItem):
+    """ Amenity Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Fasility(AbstractItem):
+    """ Fasility Model Definition """
+
+    class Meta:
+        verbose_name_plural = "Fasilities"
+
+
+class HouseRule(AbstractItem):
+    """ House Rule Model Definition """
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Rule(AbstractItem):
+    """ Rule Model Definition """
 
 
 class Room(core_models.TimeStampedModel):
@@ -20,4 +63,26 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(to=user_models.User, on_delete=models.CASCADE)
+    host = models.ForeignKey(to="users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey(
+        "RoomType",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    fasilities = models.ManyToManyField("Fasility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(core_models.TimeStampedModel):
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
