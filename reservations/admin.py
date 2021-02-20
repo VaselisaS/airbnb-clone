f2636ee2d3abf0fa512fa42b01d5b10e29700948
lisa <lisa.exz@gmail.com>
2021-02-20 21:02:04 +0200

    change admin 'reservations' and add two metods

--- reservations/admin.py
+++ reservations/admin.py
@@ -5,3 +5,15 @@ from . import models
 @admin.register(models.Reservation)
 class ReservationAdmin(admin.ModelAdmin):
     """ Reservation Admin Definition """
+
+    list_display = (
+        "room",
+        "status",
+        "check_in",
+        "check_out",
+        "guest",
+        "in_progress",
+        "in_finished",
+    )
+
+    list_filter = ("status",)
