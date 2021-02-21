c1aaf9c126e3614c8d2f6f7382471edf1bb4ab19
lisa <lisa.exz@gmail.com>
2021-02-21 16:59:59 +0200

    create Image file folder (settings file, model file)

--- users/models.py
+++ users/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 class User(AbstractUser):
     """ Custom User Model """
-    GENDER_MALE = 'male'
-    GENDER_FEMALE = 'female'
-    GENDER_OTHER = 'other'
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
 
     GENDER_CHOICES = [
         (GENDER_MALE, "Male"),
@@ -14,39 +15,33 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other"),
     ]
 
-    LANGUAGE_ENGLISH = 'en'
-    LANGUAGE_UKRAIN = 'ua'
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_UKRAIN = "ua"
 
     LANGUAGE_CHOICES = [
-        (LANGUAGE_ENGLISH, 'English'),
-        (LANGUAGE_UKRAIN, 'Ukrain'),
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_UKRAIN, "Ukrain"),
     ]
 
-    CURRENCY_USD = 'usd'
-    CURRENCY_UAH = 'uah'
+    CURRENCY_USD = "usd"
+    CURRENCY_UAH = "uah"
 
     CURRENCY_CHOICES = [
-        (CURRENCY_USD, 'USD'),
-        (CURRENCY_UAH, 'UAH'),
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_UAH, "UAH"),
     ]
 
     bio = models.TextField(blank=True)
     gender = models.CharField(
-        max_length=10,
-        choices=GENDER_CHOICES,
-        blank=True
+        max_length=10, choices=GENDER_CHOICES, blank=True
     )
-    avatar = models.ImageField(blank=True)
+    avatar = models.ImageField(blank=True, upload_to="avatars")
     birthdate = models.DateField(blank=True, null=True)
     language = models.CharField(
-        choices=LANGUAGE_CHOICES,
-        max_length=3,
-        blank=True
+        choices=LANGUAGE_CHOICES, max_length=3, blank=True
     )
 
     currency = models.CharField(
-        choices=CURRENCY_CHOICES,
-        max_length=3,
-        blank=True
+        choices=CURRENCY_CHOICES, max_length=3, blank=True
     )
     superhost = models.BooleanField(default=False)
