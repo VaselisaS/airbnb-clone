81b00b607a867bf889cdd116e18446e07fedb2db
lisa <lisa.exz@gmail.com>
2021-03-01 17:20:48 +0200

    add templates for model room

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rooms import views as views_rooms
 
+app_name = "core"
 urlpatterns = [
-    path("", views_rooms.all_rooms, name="home"),
+    path("", views_rooms.HomeView.as_view(), name="home"),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.views.generic import ListView, DetailView
 from . import models
 
 
-def all_rooms(request):
-    rooms = models.Room.objects.all()
-    paginator = Paginator(object_list=rooms, per_page=10)
-    page_number = int(request.GET.get("page", 1))
-    try:
-        page_obj = paginator.page(page_number)
-        return render(
-            request, "rooms/all_rooms.html", context={"page": page_obj}
-        )
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):
+
+    """HomeView Definition"""
+
+    model = models.Room
+    paginate_by = 10
+    ordering = "created"
+
+
+class RoomView(DetailView):
+
+    """RoomView Definition"""
+
+    model = models.Room
